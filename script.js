a80786c5c8b434871a8ebe327ea4bbf00d75884a
Rattoos <Ricky.tysar@yahoo.com>
2019-03-02 12:33:37 -0500

    test

---
+++ script.js
@@ -0,0 +1,14 @@
+const grid = document.querySelector('.grid');
+
+
+
+createGrid(16);
+blackFill();
+
+
+
+function createGrid(RowsCols){
+    for (let i = 0; i < RowsCols*RowsCols; i++) {
+        const grid = document.querySelector('.grid');
+    }
+}
