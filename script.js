cfc90653a9de852b55b3bd336c3b6612ba027278
Rattoos <Ricky.tysar@yahoo.com>
2019-03-04 09:25:44 -0500

    needs features

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-const Sketch = document.querySelector('.Sketch');
+//Define vars.
+const containerGrid = document.querySelector(".containerGrid");
+const gsize = document.querySelector('#gsize');
+const reset = document.querySelector('#reset');
+//const pride = document.getElementById("pride");
+//const black = document.getElementById("black");
 
-let grid_height = 250;
-let grayscale_mode = true;
+//grid size
+newGrid(16);
+
+//please just give me a fucking grid you piece of trash
+function newGrid(r) {
+    while(containerGrid.firstChild) {
+        containerGrid.removeChild(containerGrid.firstChild);
+    }
+    
+    containerGrid.style.setProperty('--grid', r);
+    
+    for (var i = 0; i < r; i++) {
+        for (var j = 0; j < r; j++) {
+            const dv = document.createElement('div');
+            dv.setAttribute('class', 'box');
+            containerGrid.appendChild(dv);
+        }
+    }
+    colorBox();
+}
 
 
+//mouseover plz kill me
+function colorBox() {
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', (e) => {
+            e.target.classList.remove('box');
+            e.target.classList.add('newBox');
+        });
+    });
+}
+
+//reset all the fucking things fuckkkkkkkkiiiinnnnnnng finallllyyyyyyy
+reset.addEventListener('click', () => {
+    const boxes = document.querySelectorAll(".newBox");
+    boxes.forEach(box => {
+        box.classList.remove('newBox');
+        box.classList.add('box');
+    });
+});
+
+//change size button prompt
+gsize.addEventListener('click', () => {
+    let size = parseInt(prompt("Enter size: "));
+    newGrid(size);
+});
